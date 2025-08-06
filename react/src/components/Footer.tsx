c46c29cbaea220ba70300d8a507b475f094c4b9d
tuanphan08 <tuanphan08@gmail.com>
2025-08-06 16:12:13 -0700

    Add Footer to Search (#7)

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -5,7 +5,31 @@ const Footer: React.FC = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>Footer Content - 78px Height</p>
+                <div className="footer-left">
+                    <img src="transparent-logo.png" alt="Coca-Cola Logo" className="footer-logo" />
+                </div>
+                <div className="footer-right">
+                    <p>
+                        <a href="https://assets.coke.com/content/share/us/en/help/meet-the-team.html" className="footer-link meet-team">
+                            Meet the Team
+                        </a>
+                    </p>
+                    <p>
+                        <a href="mailto:assetmanagers@coca-cola.com" className="footer-link contact-us">
+                            Contact Us
+                        </a>
+                    </p>
+                    <p>
+                        <a href="https://assets.coke.com/content/share/us/en/help/support-portal.html" className="footer-link support-training">
+                            Support & Training
+                        </a>
+                    </p>
+                    <p>
+                        <a href="https://assets.coke.com/content/share/us/en/help/faqs.html" className="footer-link faqs">
+                            FAQs
+                        </a>
+                    </p>
+                </div>
             </div>
         </footer>
     );
