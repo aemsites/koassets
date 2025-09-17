08b1e6aefbe9d04e6e864e06d19517dacbe1b835
jfait <jfait@adobe.com>
2025-09-17 18:18:09 -0400

    Test URLs:
    Before: https://main--koassets--aemsites.aem.live/
    After: https://myCollections--koassets--aemsites.aem.live/
    
    -Add myCollections page
    -Add myCollection details page
    -Add add to collection in search results

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -277,7 +277,7 @@ function createHeaderBar() {
         <li><a href="#">My Rights Requests</a></li>
         <li><a href="#">My Saved Templates</a></li>
         <li><a href="#">My Print Jobs</a></li>
-        <li><a href="#">My Collections</a></li>
+        <li><a href="/my-collections">My Collections</a></li>
         <li><a href="#">My Saved Searches</a></li>
         <li><a href="/auth/logout">Log Out</a></li>
       </ul>
