31f9ec6ae88b1624944591bf9835cc932073694f
jfait <jfait@adobe.com>
2025-10-08 23:05:19 -0400

    temp enable logging

--- scripts/collections/collections-api-client.js
+++ scripts/collections/collections-api-client.js
@@ -275,7 +275,16 @@ export class DynamicMediaCollectionsClient {
    */
   // eslint-disable-next-line class-methods-use-this
   getETag(headers) {
-    return headers?.etag || headers?.ETag || null;
+    let etag = headers?.etag || headers?.ETag || null;
+
+    // Strip W/ prefix if present (weak ETag indicator)
+    // W/"430548e5-0000-0200-0000-68e7236f0000" -> "430548e5-0000-0200-0000-68e7236f0000"
+    if (etag && etag.startsWith('W/')) {
+      console.trace('DynamicMediaCollectionsClient.getETag() - fixing weak ETag,', etag);
+      etag = etag.substring(2);
+    }
+
+    return etag;
   }
 
   // ==========================================
@@ -436,6 +445,7 @@ export class DynamicMediaCollectionsClient {
       // eslint-disable-next-line no-underscore-dangle
       const etag = currentCollection._etag;
 
+      // eslint-disable-next-line no-console
       console.trace('DynamicMediaCollectionsClient.updateCollectionMetadata() ETAG', etag);
 
       // Merge current metadata with updates (preserve all existing metadata)
